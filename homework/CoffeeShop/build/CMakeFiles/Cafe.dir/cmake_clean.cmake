9d71fc4a728c2823a24e24b476c7742be20cd88b
shakeel159 <jazibshaq@gmail>
2022-03-22 23:30:19 -0700

    Cafe Opening

---
+++ homework/CoffeeShop/build/CMakeFiles/Cafe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cafe.dir/main.cpp.o"
+  "Cafe"
+  "Cafe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cafe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
